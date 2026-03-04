81d15a78395c039b2f63406cea39d91bc225b892
Aldi Majid <aldi.majid@moovr.id>
2026-03-04 10:50:34 +0700

    add popular tv series page test

--- test/presentation/pages/popular_tv_series_page_test.mocks.dart
+++ test/presentation/pages/popular_tv_series_page_test.mocks.dart
@@ -3,17 +3,14 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i7;
-import 'dart:ui' as _i8;
+import 'dart:async' as _i4;
 
-import 'package:ditonton/common/state_enum.dart' as _i4;
-import 'package:ditonton/domain/entities/tv_series.dart' as _i5;
 import 'package:ditonton/domain/usecases/tv_series/get_popular_tv_series.dart'
     as _i2;
-import 'package:ditonton/presentation/provider/tv_series/popular_tv_series_notifier.dart'
+import 'package:ditonton/presentation/bloc/tv_series/popular_tv_series_cubit.dart'
     as _i3;
+import 'package:flutter_bloc/flutter_bloc.dart' as _i5;
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:mockito/src/dummies.dart' as _i6;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -36,15 +33,17 @@ class _FakeGetPopularTvSeries_0 extends _i1.SmartFake
     : super(parent, parentInvocation);
 }
 
-/// A class which mocks [PopularTvSeriesNotifier].
+class _FakePopularTvSeriesState_1 extends _i1.SmartFake
+    implements _i3.PopularTvSeriesState {
+  _FakePopularTvSeriesState_1(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+/// A class which mocks [PopularTvSeriesCubit].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockPopularTvSeriesNotifier extends _i1.Mock
-    implements _i3.PopularTvSeriesNotifier {
-  MockPopularTvSeriesNotifier() {
-    _i1.throwOnMissingStub(this);
-  }
-
+class MockPopularTvSeriesCubit extends _i1.Mock
+    implements _i3.PopularTvSeriesCubit {
   @override
   _i2.GetPopularTvSeries get getPopularTvSeries =>
       (super.noSuchMethod(
@@ -53,71 +52,87 @@ class MockPopularTvSeriesNotifier extends _i1.Mock
               this,
               Invocation.getter(#getPopularTvSeries),
             ),
+            returnValueForMissingStub: _FakeGetPopularTvSeries_0(
+              this,
+              Invocation.getter(#getPopularTvSeries),
+            ),
           )
           as _i2.GetPopularTvSeries);
 
   @override
-  _i4.RequestState get state =>
+  _i3.PopularTvSeriesState get state =>
       (super.noSuchMethod(
             Invocation.getter(#state),
-            returnValue: _i4.RequestState.Empty,
-          )
-          as _i4.RequestState);
-
-  @override
-  List<_i5.TvSeries> get tvSeries =>
-      (super.noSuchMethod(
-            Invocation.getter(#tvSeries),
-            returnValue: <_i5.TvSeries>[],
-          )
-          as List<_i5.TvSeries>);
-
-  @override
-  String get message =>
-      (super.noSuchMethod(
-            Invocation.getter(#message),
-            returnValue: _i6.dummyValue<String>(
+            returnValue: _FakePopularTvSeriesState_1(
               this,
-              Invocation.getter(#message),
+              Invocation.getter(#state),
+            ),
+            returnValueForMissingStub: _FakePopularTvSeriesState_1(
+              this,
+              Invocation.getter(#state),
             ),
           )
-          as String);
+          as _i3.PopularTvSeriesState);
 
   @override
-  bool get hasListeners =>
-      (super.noSuchMethod(Invocation.getter(#hasListeners), returnValue: false)
+  _i4.Stream<_i3.PopularTvSeriesState> get stream =>
+      (super.noSuchMethod(
+            Invocation.getter(#stream),
+            returnValue: _i4.Stream<_i3.PopularTvSeriesState>.empty(),
+            returnValueForMissingStub:
+                _i4.Stream<_i3.PopularTvSeriesState>.empty(),
+          )
+          as _i4.Stream<_i3.PopularTvSeriesState>);
+
+  @override
+  bool get isClosed =>
+      (super.noSuchMethod(
+            Invocation.getter(#isClosed),
+            returnValue: false,
+            returnValueForMissingStub: false,
+          )
           as bool);
 
   @override
-  _i7.Future<void> fetchPopularTvSeries() =>
+  _i4.Future<void> fetchPopularTvSeries() =>
       (super.noSuchMethod(
             Invocation.method(#fetchPopularTvSeries, []),
-            returnValue: _i7.Future<void>.value(),
-            returnValueForMissingStub: _i7.Future<void>.value(),
+            returnValue: _i4.Future<void>.value(),
+            returnValueForMissingStub: _i4.Future<void>.value(),
           )
-          as _i7.Future<void>);
+          as _i4.Future<void>);
 
   @override
-  void addListener(_i8.VoidCallback? listener) => super.noSuchMethod(
-    Invocation.method(#addListener, [listener]),
+  void emit(_i3.PopularTvSeriesState? state) => super.noSuchMethod(
+    Invocation.method(#emit, [state]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void removeListener(_i8.VoidCallback? listener) => super.noSuchMethod(
-    Invocation.method(#removeListener, [listener]),
+  void onChange(_i5.Change<_i3.PopularTvSeriesState>? change) =>
+      super.noSuchMethod(
+        Invocation.method(#onChange, [change]),
+        returnValueForMissingStub: null,
+      );
+
+  @override
+  void addError(Object? error, [StackTrace? stackTrace]) => super.noSuchMethod(
+    Invocation.method(#addError, [error, stackTrace]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void dispose() => super.noSuchMethod(
-    Invocation.method(#dispose, []),
+  void onError(Object? error, StackTrace? stackTrace) => super.noSuchMethod(
+    Invocation.method(#onError, [error, stackTrace]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void notifyListeners() => super.noSuchMethod(
-    Invocation.method(#notifyListeners, []),
-    returnValueForMissingStub: null,
-  );
+  _i4.Future<void> close() =>
+      (super.noSuchMethod(
+            Invocation.method(#close, []),
+            returnValue: _i4.Future<void>.value(),
+            returnValueForMissingStub: _i4.Future<void>.value(),
+          )
+          as _i4.Future<void>);
 }
--- test/presentation/pages/tv_series_detail_page_test.dart
+++ test/presentation/pages/tv_series_detail_page_test.dart
@@ -1,25 +1,15 @@
 import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/tv_series.dart';
 import 'package:ditonton/domain/entities/tv_series_detail.dart';
-import 'package:ditonton/domain/usecases/tv_series/get_tv_series_detail.dart';
-import 'package:ditonton/domain/usecases/tv_series/get_tv_series_recommendations.dart';
-import 'package:ditonton/domain/usecases/tv_series/get_watchlist_tv_series_status.dart';
-import 'package:ditonton/domain/usecases/tv_series/remove_watchlist_tv_series.dart';
-import 'package:ditonton/domain/usecases/tv_series/save_watchlist_tv_series.dart';
 import 'package:ditonton/presentation/bloc/tv_series/tv_series_detail_cubit.dart';
-import 'package:ditonton/presentation/pages/movies/movie_detail_page.dart';
 import 'package:ditonton/presentation/pages/tv_series/tv_series_detail_page.dart';
-import 'package:ditonton/presentation/provider/movie/movie_detail_notifier.dart';
-import 'package:ditonton/presentation/provider/tv_series/tv_series_detail_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:provider/provider.dart';
 
 import '../../dummy_data/dummy_objects.dart';
 import 'tv_series_detail_page_test.mocks.dart';
@@ -130,83 +120,4 @@ void main() {
 
     expect(find.text('Server Failure'), findsOneWidget);
   });
-
-  //
-  // testWidgets(
-  //     'Watchlist button should display add icon when tvSeries not added to watchlist',
-  //     (WidgetTester tester) async {
-  //   when(mockNotifier.tvSeriesState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeries).thenReturn(testTvSeriesDetail);
-  //   when(mockNotifier.recommendationState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeriesRecommendations).thenReturn(<TvSeries>[]);
-  //   when(mockNotifier.isAddedToWatchlist).thenReturn(false);
-  //
-  //   final watchlistButtonIcon = find.byIcon(Icons.add);
-  //
-  //   await tester.pumpWidget(_makeTestableWidget(TvSeriesDetailPage(id: 1)));
-  //
-  //   expect(watchlistButtonIcon, findsOneWidget);
-  // });
-  //
-  // testWidgets(
-  //     'Watchlist button should display check icon when tvSeries is added to wathclist',
-  //     (WidgetTester tester) async {
-  //   when(mockNotifier.tvSeriesState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeries).thenReturn(testTvSeriesDetail);
-  //   when(mockNotifier.recommendationState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeriesRecommendations).thenReturn(<TvSeries>[]);
-  //   when(mockNotifier.isAddedToWatchlist).thenReturn(true);
-  //
-  //   final watchlistButtonIcon = find.byIcon(Icons.check);
-  //
-  //   await tester.pumpWidget(_makeTestableWidget(TvSeriesDetailPage(id: 1)));
-  //
-  //   expect(watchlistButtonIcon, findsOneWidget);
-  // });
-  //
-  // testWidgets(
-  //     'Watchlist button should display Snackbar when added to watchlist',
-  //     (WidgetTester tester) async {
-  //   when(mockNotifier.tvSeriesState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeries).thenReturn(testTvSeriesDetail);
-  //   when(mockNotifier.recommendationState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeriesRecommendations).thenReturn(<TvSeries>[]);
-  //   when(mockNotifier.isAddedToWatchlist).thenReturn(false);
-  //   when(mockNotifier.watchlistMessage).thenReturn('Added to Watchlist');
-  //
-  //   final watchlistButton = find.byType(ElevatedButton);
-  //
-  //   await tester.pumpWidget(_makeTestableWidget(TvSeriesDetailPage(id: 1)));
-  //
-  //   expect(find.byIcon(Icons.add), findsOneWidget);
-  //
-  //   await tester.tap(watchlistButton);
-  //   await tester.pump();
-  //
-  //   expect(find.byType(SnackBar), findsOneWidget);
-  //   expect(find.text('Added to Watchlist'), findsOneWidget);
-  // });
-  //
-  // testWidgets(
-  //     'Watchlist button should display AlertDialog when add to watchlist failed',
-  //     (WidgetTester tester) async {
-  //   when(mockNotifier.tvSeriesState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeries).thenReturn(testTvSeriesDetail);
-  //   when(mockNotifier.recommendationState).thenReturn(RequestState.Loaded);
-  //   when(mockNotifier.tvSeriesRecommendations).thenReturn(<TvSeries>[]);
-  //   when(mockNotifier.isAddedToWatchlist).thenReturn(false);
-  //   when(mockNotifier.watchlistMessage).thenReturn('Failed');
-  //
-  //   final watchlistButton = find.byType(ElevatedButton);
-  //
-  //   await tester.pumpWidget(_makeTestableWidget(TvSeriesDetailPage(id: 1)));
-  //
-  //   expect(find.byIcon(Icons.add), findsOneWidget);
-  //
-  //   await tester.tap(watchlistButton);
-  //   await tester.pump();
-  //
-  //   expect(find.byType(AlertDialog), findsOneWidget);
-  //   expect(find.text('Failed'), findsOneWidget);
-  // });
 }
