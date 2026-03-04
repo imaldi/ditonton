f0ce8415335c7f73e1dc51fa81d8a96f7aa68fc6
Aldi Majid <aldi.majid@moovr.id>
2026-03-04 11:34:34 +0700

    add top rated tv series page test

--- test/presentation/pages/top_rated_tv_series_page_test.dart
+++ test/presentation/pages/top_rated_tv_series_page_test.dart
@@ -1,68 +1,80 @@
 import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/presentation/bloc/tv_series/top_rated_tv_series_cubit.dart';
 import 'package:ditonton/presentation/pages/movies/top_rated_movies_page.dart';
 import 'package:ditonton/presentation/pages/tv_series/top_rated_tv_series_page.dart';
 import 'package:ditonton/presentation/provider/tv_series/top_rated_tv_series_notifier.dart';
+import 'package:ditonton/presentation/widgets/tv_series_card_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 
+import '../../dummy_data/dummy_objects.dart';
 import 'top_rated_tv_series_page_test.mocks.dart';
 
-@GenerateMocks([TopRatedTvSeriesNotifier])
+@GenerateNiceMocks([MockSpec<TopRatedTvSeriesCubit>()])
 void main() {
-  late MockTopRatedTvSeriesNotifier mockNotifier;
+  late MockTopRatedTvSeriesCubit mockCubit;
 
   setUp(() {
-    mockNotifier = MockTopRatedTvSeriesNotifier();
+    mockCubit = MockTopRatedTvSeriesCubit();
   });
 
-  Widget _makeTestableWidget(Widget body) {
-    return ChangeNotifierProvider<TopRatedTvSeriesNotifier>.value(
-      value: mockNotifier,
+  Widget makeTestableWidget(Widget body) {
+    return BlocProvider<TopRatedTvSeriesCubit>.value(
+      value: mockCubit,
       child: MaterialApp(
         home: body,
       ),
     );
   }
 
+
   testWidgets('Page should display progress bar when loading',
-      (WidgetTester tester) async {
-    when(mockNotifier.state).thenReturn(RequestState.Loading);
+          (WidgetTester tester) async {
+        when(mockCubit.state).thenReturn(
+          const TopRatedTvSeriesState(state: RequestState.Loading),
+        );
 
-    final progressFinder = find.byType(CircularProgressIndicator);
-    final centerFinder = find.byType(Center);
+        await tester.pumpWidget(makeTestableWidget(TopRatedTvSeriesPage()));
 
-    await tester.pumpWidget(_makeTestableWidget(TopRatedTvSeriesPage()));
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+        expect(find.byType(Center), findsOneWidget);
+      });
 
-    expect(centerFinder, findsOneWidget);
-    expect(progressFinder, findsOneWidget);
-  });
+  testWidgets('Page should display ListView when data is loaded',
+          (WidgetTester tester) async {
+        when(mockCubit.state).thenReturn(
+          TopRatedTvSeriesState(
+            state: RequestState.Loaded,
+            tvSeries: testTvSeriesList,
+          ),
+        );
 
-  testWidgets('Page should display when data is loaded',
-      (WidgetTester tester) async {
-    when(mockNotifier.state).thenReturn(RequestState.Loaded);
-    when(mockNotifier.tvSeries).thenReturn(<TvSeries>[]);
+        await tester.pumpWidget(makeTestableWidget(TopRatedTvSeriesPage()));
 
-    final listViewFinder = find.byType(ListView);
-
-    await tester.pumpWidget(_makeTestableWidget(TopRatedTvSeriesPage()));
-
-    expect(listViewFinder, findsOneWidget);
-  });
+        expect(find.byType(ListView), findsOneWidget);
+        expect(find.byType(TvSeriesCard), findsWidgets);
+      });
 
   testWidgets('Page should display text with message when Error',
-      (WidgetTester tester) async {
-    when(mockNotifier.state).thenReturn(RequestState.Error);
-    when(mockNotifier.message).thenReturn('Error message');
+          (WidgetTester tester) async {
+        when(mockCubit.state).thenReturn(
+          const TopRatedTvSeriesState(
+            state: RequestState.Error,
+            message: 'Error message',
+          ),
+        );
 
-    final textFinder = find.byKey(Key('error_message'));
+        await tester.pumpWidget(makeTestableWidget(TopRatedTvSeriesPage()));
 
-    await tester.pumpWidget(_makeTestableWidget(TopRatedTvSeriesPage()));
+        final textFinder = find.byKey(const Key('error_message'));
 
-    expect(textFinder, findsOneWidget);
-  });
+        expect(textFinder, findsOneWidget);
+        expect(find.text('Error message'), findsOneWidget);
+      });
 }
--- test/presentation/pages/top_rated_tv_series_page_test.mocks.dart
+++ test/presentation/pages/top_rated_tv_series_page_test.mocks.dart
@@ -3,17 +3,14 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i7;
-import 'dart:ui' as _i8;
+import 'dart:async' as _i4;
 
-import 'package:ditonton/common/state_enum.dart' as _i4;
-import 'package:ditonton/domain/entities/tv_series.dart' as _i5;
 import 'package:ditonton/domain/usecases/tv_series/get_top_rated_tv_series.dart'
     as _i2;
-import 'package:ditonton/presentation/provider/tv_series/top_rated_tv_series_notifier.dart'
+import 'package:ditonton/presentation/bloc/tv_series/top_rated_tv_series_cubit.dart'
     as _i3;
+import 'package:flutter_bloc/flutter_bloc.dart' as _i5;
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:mockito/src/dummies.dart' as _i6;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -36,15 +33,17 @@ class _FakeGetTopRatedTvSeries_0 extends _i1.SmartFake
     : super(parent, parentInvocation);
 }
 
-/// A class which mocks [TopRatedTvSeriesNotifier].
+class _FakeTopRatedTvSeriesState_1 extends _i1.SmartFake
+    implements _i3.TopRatedTvSeriesState {
+  _FakeTopRatedTvSeriesState_1(Object parent, Invocation parentInvocation)
+    : super(parent, parentInvocation);
+}
+
+/// A class which mocks [TopRatedTvSeriesCubit].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockTopRatedTvSeriesNotifier extends _i1.Mock
-    implements _i3.TopRatedTvSeriesNotifier {
-  MockTopRatedTvSeriesNotifier() {
-    _i1.throwOnMissingStub(this);
-  }
-
+class MockTopRatedTvSeriesCubit extends _i1.Mock
+    implements _i3.TopRatedTvSeriesCubit {
   @override
   _i2.GetTopRatedTvSeries get getTopRatedTvSeries =>
       (super.noSuchMethod(
@@ -53,71 +52,87 @@ class MockTopRatedTvSeriesNotifier extends _i1.Mock
               this,
               Invocation.getter(#getTopRatedTvSeries),
             ),
+            returnValueForMissingStub: _FakeGetTopRatedTvSeries_0(
+              this,
+              Invocation.getter(#getTopRatedTvSeries),
+            ),
           )
           as _i2.GetTopRatedTvSeries);
 
   @override
-  _i4.RequestState get state =>
+  _i3.TopRatedTvSeriesState get state =>
       (super.noSuchMethod(
             Invocation.getter(#state),
-            returnValue: _i4.RequestState.Empty,
-          )
-          as _i4.RequestState);
-
-  @override
-  List<_i5.TvSeries> get tvSeries =>
-      (super.noSuchMethod(
-            Invocation.getter(#tvSeries),
-            returnValue: <_i5.TvSeries>[],
-          )
-          as List<_i5.TvSeries>);
-
-  @override
-  String get message =>
-      (super.noSuchMethod(
-            Invocation.getter(#message),
-            returnValue: _i6.dummyValue<String>(
+            returnValue: _FakeTopRatedTvSeriesState_1(
               this,
-              Invocation.getter(#message),
+              Invocation.getter(#state),
+            ),
+            returnValueForMissingStub: _FakeTopRatedTvSeriesState_1(
+              this,
+              Invocation.getter(#state),
             ),
           )
-          as String);
+          as _i3.TopRatedTvSeriesState);
 
   @override
-  bool get hasListeners =>
-      (super.noSuchMethod(Invocation.getter(#hasListeners), returnValue: false)
+  _i4.Stream<_i3.TopRatedTvSeriesState> get stream =>
+      (super.noSuchMethod(
+            Invocation.getter(#stream),
+            returnValue: _i4.Stream<_i3.TopRatedTvSeriesState>.empty(),
+            returnValueForMissingStub:
+                _i4.Stream<_i3.TopRatedTvSeriesState>.empty(),
+          )
+          as _i4.Stream<_i3.TopRatedTvSeriesState>);
+
+  @override
+  bool get isClosed =>
+      (super.noSuchMethod(
+            Invocation.getter(#isClosed),
+            returnValue: false,
+            returnValueForMissingStub: false,
+          )
           as bool);
 
   @override
-  _i7.Future<void> fetchTopRatedTvSeries() =>
+  _i4.Future<void> fetchTopRatedTvSeries() =>
       (super.noSuchMethod(
             Invocation.method(#fetchTopRatedTvSeries, []),
-            returnValue: _i7.Future<void>.value(),
-            returnValueForMissingStub: _i7.Future<void>.value(),
+            returnValue: _i4.Future<void>.value(),
+            returnValueForMissingStub: _i4.Future<void>.value(),
           )
-          as _i7.Future<void>);
+          as _i4.Future<void>);
 
   @override
-  void addListener(_i8.VoidCallback? listener) => super.noSuchMethod(
-    Invocation.method(#addListener, [listener]),
+  void emit(_i3.TopRatedTvSeriesState? state) => super.noSuchMethod(
+    Invocation.method(#emit, [state]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void removeListener(_i8.VoidCallback? listener) => super.noSuchMethod(
-    Invocation.method(#removeListener, [listener]),
+  void onChange(_i5.Change<_i3.TopRatedTvSeriesState>? change) =>
+      super.noSuchMethod(
+        Invocation.method(#onChange, [change]),
+        returnValueForMissingStub: null,
+      );
+
+  @override
+  void addError(Object? error, [StackTrace? stackTrace]) => super.noSuchMethod(
+    Invocation.method(#addError, [error, stackTrace]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void dispose() => super.noSuchMethod(
-    Invocation.method(#dispose, []),
+  void onError(Object? error, StackTrace? stackTrace) => super.noSuchMethod(
+    Invocation.method(#onError, [error, stackTrace]),
     returnValueForMissingStub: null,
   );
 
   @override
-  void notifyListeners() => super.noSuchMethod(
-    Invocation.method(#notifyListeners, []),
-    returnValueForMissingStub: null,
-  );
+  _i4.Future<void> close() =>
+      (super.noSuchMethod(
+            Invocation.method(#close, []),
+            returnValue: _i4.Future<void>.value(),
+            returnValueForMissingStub: _i4.Future<void>.value(),
+          )
+          as _i4.Future<void>);
 }
